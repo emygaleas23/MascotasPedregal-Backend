79fae9c18daa6ae29cb3de1900c3e09b74626872
emygaleas23 <emily.galeas23@gmail.com>
2026-04-24 04:52:59 -0500

    Modelo Mascota

---
+++ src/models/mascotas/Mascota.js
@@ -0,0 +1,47 @@
+import {Schema, model} from "mongoose";
+
+const Mascota = new Schema(
+    {
+        owner_id:{
+            type: Schema.Types.ObjectId,
+            ref:"Usuario",
+            required: true
+        },
+        nombre: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        tipo: {
+            type: String,
+            enum:["Perro", "Gato", "Otro"],
+            required: true
+        },
+        raza: {
+            type: String
+        },
+        genero: {
+            type: String,
+            enum: ["M", "H"]
+        },
+        tamano: {
+            type: String,
+            enum: ["Pequeño", "Mediano", "Grande"]
+        },
+        color: {
+            type: String
+        },
+        fecha_nacimiento: {
+            type: Date
+        },
+        foto_principal: {
+            type: String
+        },
+        descripcion: {
+            type: String
+        }
+    },
+    {
+        timestamps:true
+    }
+)
