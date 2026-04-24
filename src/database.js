d78de98a7bfc10a36a6bb7f7a16a572faee5af4a
emygaleas23 <emily.galeas23@gmail.com>
2026-04-24 05:27:45 -0500

    Cambios BDD

--- src/database.js
+++ src/database.js
@@ -7,11 +7,14 @@ mongoose.set('strictQuery', true);
 
 const connection = async () => {
     try {
+        if(!process.env.MONGODB_URI_ATLAS){
+            console.error("ERROR: La varianl MONGODB_URI_ATLAS no está definida")
+            return
+        }
         const {connection} = await mongoose.connect(process.env.MONGODB_URI_ATLAS)
         console.log(`Conexión a la base de datos exitosa en "${connection.host} - ${connection.port}"`)
     } catch (error) {
         console.log("Error al conectar a la base de datos:", error)
-        process.exit(1)
     }
 }
 
