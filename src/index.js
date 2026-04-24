7ffc79db98b063f24efa0fbc5a1c40e6005e0e7e
emygaleas23 <emily.galeas23@gmail.com>
2026-04-24 09:55:16 -0500

    Cambios al iniciar el servidor

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import app from './server.js'
 import connection from './database.js'
 
 // Iniciar el servidor
-
-app.listen(app.get('port'),()=>{
-    console.log(`Servidor iniciado en el puerto ${app.get('port')}`)
-})
-
-// Conectar a la base de datos
-connection()
+(async ()=>{
+    try{
+        await connection();
+        const port = app.get("port") || 3000;
+        app.listen(port,()=>{
+            console.log(
+                `Ejecutándose en: http://localhost:${port}`
+            )
+        });
+    } catch (error){
+        console.error("❌ Error al iniciar servidor:", error);
+        process.exit(1);
+    }
+})();
